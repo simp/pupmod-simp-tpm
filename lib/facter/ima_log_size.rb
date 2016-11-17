787ff94b88d66ff59a7398b21b4bff9fde055f57
Nick Miller <nick.miller@onyxpoint.com>
2016-11-17 10:08:51 -0500

    (SIMP-2044) Properly confine ima_log_size
    Add a confine to the ima_log_size fact to make sure the file exists
    before checking the size.
    
    SIMP-2044 #close

--- lib/facter/ima_log_size.rb
+++ lib/facter/ima_log_size.rb
@@ -1,5 +1,7 @@
 # Detects the size if the IMA log in bytes
 Facter.add('ima_log_size') do
+  confine :exists => '/sys/kernel/security/ima/ascii_runtime_measurements'
+
   setcode do
     Facter::Core::Execution.execute('wc -c /sys/kernel/security/ima/ascii_runtime_measurements').to_i
   end
