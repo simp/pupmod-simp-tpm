f83b312e13ce31dac7bd00a33d120a8c452000a3
Nick Miller <nick.miller@onyxpoint.com>
2016-11-17 10:08:51 -0500

    (SIMP-2044) Check securityfs in ima_log_size
    Add a check to the ima_log_size fact to make sure securityfs and the
    file needed exists before checking the size of it
    
    SIMP-2044 #close

--- lib/facter/ima_log_size.rb
+++ lib/facter/ima_log_size.rb
@@ -1,6 +1,10 @@
 # Detects the size if the IMA log in bytes
 Facter.add('ima_log_size') do
   setcode do
-    Facter::Core::Execution.execute('wc -c /sys/kernel/security/ima/ascii_runtime_measurements').to_i
+    f = "/sys/kernel/security/ima/ascii_runtime_measurements"
+
+    if File.exists? f
+      Facter::Core::Execution.execute("wc -c #{f}").to_i
+    end
   end
 end
