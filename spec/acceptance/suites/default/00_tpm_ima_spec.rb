3d22d15818d62e239ecf7acf80bcfbb5068163ce
Nick Miller <nick.miller@onyxpoint.com>
2018-01-11 13:21:03 -0500

    (SIMP-4236) ima: Soup up acceptance test
    SIMP-4236 #close

--- spec/acceptance/suites/default/00_tpm_ima_spec.rb
+++ spec/acceptance/suites/default/00_tpm_ima_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper_acceptance'
 require 'json'
 
+# module Beaker
+#   class SshConnection
+#     CATCHABLE_EXCEPTIONS = RETRYABLE_EXCEPTIONS
+#     RETRYABLE_EXCEPTIONS = []
+#   end
+# end
+
+def try_to_ssh(host, user, opts)
+  begin
+    out = Net::SSH.start(host.connection.ip, user, opts).exec!('ls')
+  rescue Net::SSH::ConnectionTimeout
+    out = 'connection timeout'
+  end
+  out
+end
+
 test_name 'tpm::ima class'
 
 describe 'tpm::ima class' do
@@ -15,9 +31,12 @@ describe 'tpm::ima class' do
     hosts.each do |host|
       manifest = <<-EOF
         include 'tpm'
-        class { 'tpm::ima':
-          manage_policy => true
-        }
+        include 'tpm::ima'
+        include 'tpm::ima::policy'
+        # class { 'tpm::ima::policy':
+        #   set_with_service => false,
+        #   set_with_puppet  => false
+        # }
       EOF
 
       it 'should run puppet' do
@@ -68,64 +87,30 @@ describe 'tpm::ima class' do
         apply_manifest_on(host, manifest, catch_changes: true)
       end
 
+      it 'locks up the filesystem after a reboot and new policy is applied' do
+        # save ssh configuration, it should be the same post-reboot
+        ssh_user = host.connection.instance_variable_get(:@user)
+        ssh_opts = host[:ssh]
+        host.connection.ssh_connection_preference = [:ip]
+        expect(on(host, 'ls')).to be_truthy
 
-      it 'should run puppet idempotently after a reboot' do
-        # reboot to apply kernel_parameter settings
+        # test the saved config
+        test = try_to_ssh(host, ssh_user, ssh_opts)
+        expect(test).not_to match 'connection timeout'
+
         host.reboot
-        # the mount will need to be reset
-        apply_manifest_on(host, manifest, catch_failures: true)
 
-        apply_manifest_on(host, manifest, catch_changes: true)
-      end
+        # class BigTimeoutError < Timeout::Error; end
 
-      it 'should not lock up the filesystem' do
-        on(host, "cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 1000 | head -n 10000 > /root/hugefile")
-        on(host, 'head -15 /sys/kernel/security/ima/ascii_runtime_measurements')
-        on(host, 'ls -la ~')
-      end
-    end
-  end
+        sleep 30
 
-  context 'even more strict rules' do
-    hosts.each do |host|
-      manifest = <<-EOF
-        include 'tpm'
-        include 'tpm::ima'
-        class { 'tpm::ima::policy':
-          measure_root_read_files    => true,
-          measure_file_mmap          => true,
-          measure_bprm_check         => true,
-          measure_module_check       => true,
-          appraise_fowner            => true,
-          dont_watch_rpm_var_cache_t => false,
-          dont_watch_puppet_log_t    => false,
-          dont_watch_lastlog_t       => false,
-        }
-      EOF
+        # require 'pry';binding.pry
 
-      it 'should run puppet' do
-        apply_manifest_on(host, manifest, catch_failures: true)
-      end
+        expect(on(host, 'ls')).to raise_error(/Cannot connect to/)
 
-      it 'should run puppet idempotently' do
-        apply_manifest_on(host, manifest, catch_changes: true)
-      end
-
-
-      it 'should run puppet idempotently after a reboot' do
-        # reboot to apply kernel_parameter settings
-        host.reboot
-        # the mount will need to be reset
-        apply_manifest_on(host, manifest, catch_failures: true)
-
-        apply_manifest_on(host, manifest, catch_changes: true)
-      end
-
-      it 'may lock up the filesystem' do
-        on(host, 'yum install -y tree')
-        host.reboot
-        on(host, 'head -15 /sys/kernel/security/ima/ascii_runtime_measurements')
-        on(host, 'ls -la ~')
+        # use the saved config, but expect it to fail
+        test2 = try_to_ssh(host, ssh_user, ssh_opts)
+        expect(test2).to match 'connection timeout'
       end
     end
   end
