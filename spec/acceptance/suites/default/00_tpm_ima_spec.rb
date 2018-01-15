225e91771ea316d374b71955860ecedb82c1bfc7
Nick Miller <nick.miller@onyxpoint.com>
2018-01-15 13:50:29 -0500

    (SIMP-4236) ima: Soup up acceptance test (#47)
    SIMP-4236 #close

--- spec/acceptance/suites/default/00_tpm_ima_spec.rb
+++ spec/acceptance/suites/default/00_tpm_ima_spec.rb
@@ -15,9 +15,12 @@ describe 'tpm::ima class' do
     hosts.each do |host|
       manifest = <<-EOF
         include 'tpm'
-        class { 'tpm::ima':
-          manage_policy => true
-        }
+        include 'tpm::ima'
+        include 'tpm::ima::policy'
+        # class { 'tpm::ima::policy':
+        #   set_with_service => false,
+        #   set_with_puppet  => false
+        # }
       EOF
 
       it 'should run puppet' do
@@ -68,64 +71,29 @@ describe 'tpm::ima class' do
         apply_manifest_on(host, manifest, catch_changes: true)
       end
 
+      it 'locks up the filesystem after a reboot and new policy is applied' do
+        on(host, 'yum install -y telnet')
+        ssh_config = File.readlines(host[:ssh][:config])
+        ssh_port   = ssh_config.grep(/port/i).first.split(' ')[1]
 
-      it 'should run puppet idempotently after a reboot' do
-        # reboot to apply kernel_parameter settings
+        expect(on(host, 'ls')).to be_truthy
+
+        tel = Net::Telnet::new("Port" => ssh_port)
+        result = tel.cmd('echo echo')
+        tel.close
+        expect(result).to match(/OpenSSH/)
+
         host.reboot
-        # the mount will need to be reset
-        apply_manifest_on(host, manifest, catch_failures: true)
+        sleep 30
 
-        apply_manifest_on(host, manifest, catch_changes: true)
-      end
-
-      it 'should not lock up the filesystem' do
-        on(host, "cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 1000 | head -n 10000 > /root/hugefile")
-        on(host, 'head -15 /sys/kernel/security/ima/ascii_runtime_measurements')
-        on(host, 'ls -la ~')
-      end
-    end
-  end
-
-  context 'even more strict rules' do
-    hosts.each do |host|
-      manifest = <<-EOF
-        include 'tpm'
-        include 'tpm::ima'
-        class { 'tpm::ima::policy':
-          measure_root_read_files    => true,
-          measure_file_mmap          => true,
-          measure_bprm_check         => true,
-          measure_module_check       => true,
-          appraise_fowner            => true,
-          dont_watch_rpm_var_cache_t => false,
-          dont_watch_puppet_log_t    => false,
-          dont_watch_lastlog_t       => false,
-        }
-      EOF
-
-      it 'should run puppet' do
-        apply_manifest_on(host, manifest, catch_failures: true)
-      end
-
-      it 'should run puppet idempotently' do
-        apply_manifest_on(host, manifest, catch_changes: true)
-      end
-
-
-      it 'should run puppet idempotently after a reboot' do
-        # reboot to apply kernel_parameter settings
-        host.reboot
-        # the mount will need to be reset
-        apply_manifest_on(host, manifest, catch_failures: true)
-
-        apply_manifest_on(host, manifest, catch_changes: true)
-      end
-
-      it 'may lock up the filesystem' do
-        on(host, 'yum install -y tree')
-        host.reboot
-        on(host, 'head -15 /sys/kernel/security/ima/ascii_runtime_measurements')
-        on(host, 'ls -la ~')
+        tel2 = Net::Telnet::new("Port" => ssh_port)
+        begin
+          result2 = tel.cmd('echo echo')
+        rescue IOError => e
+          result2 = e
+        end
+        tel2.close
+        expect(result2).to be_instance_of(IOError)
       end
     end
   end
